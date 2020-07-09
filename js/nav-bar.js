a72674a9d931facc9267e3589122e7816b6ddd5a
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2020-07-09 11:02:25 -0700

    finished blog project

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,9 +1,11 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function handleClicked() {
-    var elem = document.getElementById("myTopnav");
-    if (elem.className === "topnav") {
-      elem.className += " responsive";
-    } else {
-      elem.className = "topnav";
-    }
+  var elem = document.getElementById("myTopnav");
+  /* first click: add reponsive class */
+  if (elem.className === "topnav") {
+    elem.className += " responsive";
+  } else {
+    /* second click: remove responsive class */
+    elem.className = "topnav";
   }
+}
